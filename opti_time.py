4cb3539ff7cea8a11aaa1d65bfa3c547bc9b7aac
ThalyaC1 <nathalie.mamain@yahoo.fr>
2024-06-14 17:48:23 +0200

    Nettoyage

--- opti_time.py
+++ opti_time.py
@@ -1,17 +1,13 @@
 from time import perf_counter
-
-from optimized import placement_optimized
-from csv_cleaner import clean_data
-
-def optimiz_csv(fichier, portefeuille):
-    data_cleaned = clean_data(fichier)
-    placement_optimized(portefeuille, data_cleaned)
-
-start_time = perf_counter()
+from optimized_csv import optimiz_csv
 
 portefeuille = 500
+profit_pourcent_min = input("Pour chaque action, quel pourcentage minimum de profit souhaitez-vous?")
 fichier = "dataset1_Python+P7.csv"
-optimiz_csv(fichier, portefeuille)
+#"data_essai.csv"
+#"dataset2_Python+P7.csv"
+start_time = perf_counter()
+optimiz_csv(fichier, portefeuille, profit_pourcent_min)
 
 end_time = perf_counter()
 time_execution_prog = end_time - start_time
