f56d1c3389359f18b66def4d65ae8e935768ea92
ThalyaC1 <nathalie.mamain@yahoo.fr>
2024-06-14 17:46:45 +0200

    changement profit_pourcent_min

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -13,8 +13,9 @@ def lire_csv(fichier):
 
 #lire_csv("dataset2_Python+P7.csv")
 
-def clean_data(fichier):
+def clean_data(fichier, profit_pourcent_min):
     data = lire_csv(fichier)
+    data_nbre_debut = len(data)-1
     data_cleaned = []
     for ligne in data :
         if len(ligne) != 3:
@@ -25,12 +26,12 @@ def clean_data(fichier):
             profit = float(ligne[2])
         except ValueError:
             continue
-        if prix < 0 or profit <=0:
+        if prix < 0 or profit <=int(profit_pourcent_min):
             continue
         action = {"name":nom_action, "price":prix, "profit": profit}
         data_cleaned.append(action)
     #print(data_cleaned)
-    print(len(data_cleaned))
+    print(f"Le fichier contient {data_nbre_debut} actions, {len(data_cleaned)} sont exploitées pour obtenir le résultat suivant")
     return data_cleaned
 
 #clean_data("dataset1_Python+P7.csv")
